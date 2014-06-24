baafe8e8ad8dce4b0f7bfb15ef8b7e7e2726cf52
Souleiman Ayoub <souleiman@hxdcml.com>
2014-06-23 21:49:10 -0400

    Initial Database is setup

---
+++ sql/tables.sql
@@ -0,0 +1,23 @@
+CREATE TABLE IF NOT EXISTS PROFILE (
+    id              SERIAL PRIMARY KEY,
+    username        VARCHAR(32) NOT NULL UNIQUE,
+    level           INT DEFAULT 0 CHECK (level >= 0),  -- Administrative level
+    last_seen       DATE
+);
+
+CREATE TABLE IF NOT EXISTS CONTENT (
+    id              SERIAL PRIMARY KEY,
+    profile_id      INT REFERENCES PROFILE(id),
+    title           VARCHAR(32) NOT NULL,
+    subject         VARCHAR(160) NOT NULL,
+    image           VARCHAR(255),
+    last_modified   DATE,
+    date_created    DATE
+);
+
+CREATE TABLE IF NOT EXISTS TAG (
+    id              SERIAL PRIMARY KEY,
+    content_id      INT REFERENCES CONTENT(id),
+    tag             VARCHAR(32),
+    count           INT DEFAULT 1
+);
